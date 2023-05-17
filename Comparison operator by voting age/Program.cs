84333f2f492f71f9133622fc64700b2ce2532911
user <user@DESKTOP-4OE0EQ1>
2023-05-17 23:19:42 +0100

    Add project files.

---
+++ Comparison operator by voting age/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Comparison_operator_by_voting_age
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Hello World!");
+
+            int myage = 25;
+            int votingage = 18;
+
+            Console.WriteLine(myage >= votingage);
+
+            if (myage >= votingage)
+            {
+                Console.WriteLine("old enough to vote");
+
+            }
+            else
+            {
+                Console.WriteLine("not old enough to vote");
+            }
+
+
+        }
+
+    }
+}
+
+        
